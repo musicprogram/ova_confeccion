2bba4009002db807c07875f8711df9390db6fc65
musicprogram <juan-o-henao@hotmail.es>
2018-04-16 16:30:51 -0500

    arreglando diseño

---
+++ pages/inicio/dashboard.php
@@ -0,0 +1,140 @@
+<script>
+  (function($){
+    $(function(){
+
+
+      $('.tooltipped').tooltip();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+</script>
+
+<div class="row">
+  <div class="col s12 m4">
+
+    <div id="card-1" class="card ">
+      <div class="card-content separar center-align tooltipped" data-position="left" data-tooltip="PATRONAJE">
+        <header>
+
+          <label class="title deep-purple-text text-darken-1 ">PATRONAJE</label>
+        </header>
+        <main>
+         
+          <img src="images/machine.png" alt="" class="responsive-img centrar">
+
+        </main>
+
+      </div>
+    </div>
+
+
+  </div>
+  <div class="col s12 m4">
+   <div id="card-1" class="card ">
+    <div class="card-content separar center-align tooltipped" data-position="top" data-tooltip="CONFECCIÓN">
+      <header>
+
+        <label class="title deep-purple-text text-darken-1 ">CONFECCIÓN</label>
+      </header>
+      <main>
+       
+        <img src="images/machine_blue.png" alt="" class="responsive-img centrar">
+
+      </main>
+
+    </div>
+  </div>
+
+</div>
+
+
+
+
+<div class="col s12 m4">
+
+
+ <div id="card-1" class="card ">
+  <div class="card-content separar center-align tooltipped" data-position="right" data-tooltip="TRAZO Y CORTE">
+    <header>
+
+      <label class="title deep-purple-text text-darken-1 ">TRAZO Y CORTE</label>
+    </header>
+    <main>
+     
+      <img src="images/ic_content_cut_128.png" alt="" class="responsive-img centrar">
+
+    </main>
+
+  </div>
+</div>
+
+
+</div>
+
+</div>
+
+<div class="row">
+  <div class="col s12 m4">
+
+
+   <div id="card-1" class="card ">
+    <div class="card-content separar center-align tooltipped" data-position="left" data-tooltip="FICHA TÉCNICA">
+      <header>
+
+        <label class="title deep-purple-text text-darken-1 ">FICHA TÉCNICA</label>
+      </header>
+      <main>
+       
+        <img src="images/ic_assignment_turned_in_128.png" alt="" class="responsive-img centrar">
+
+      </main>
+
+    </div>
+  </div>
+
+
+</div>
+
+<div class="col s12 m4">
+
+
+ <div id="card-1" class="card ">
+  <div class="card-content separar center-align tooltipped" data-position="bottom" data-tooltip="EMPRESA">
+    <header>
+
+      <label class="title deep-purple-text text-darken-1 ">EMPRESA</label>
+    </header>
+    <main>
+     
+      <img src="images/ic_wallet_travel_128.png" alt="" class="responsive-img centrar">
+
+    </main>
+
+  </div>
+</div>
+
+
+</div>
+
+<div class="col s12 m4">
+
+
+ <div id="card-1" class="card ">
+  <div class="card-content separar center-align tooltipped" data-position="right" data-tooltip="FORO">
+    <header>
+
+      <label class="title deep-purple-text text-darken-1 ">FORO</label>
+    </header>
+    <main>
+     
+      <img src="images/ic_chat_128.png" alt="" class="responsive-img centrar">
+
+    </main>
+
+  </div>
+</div>
+
+
+</div>
+</div>
---
+++ pages/inicio/nav_inicio.php
@@ -0,0 +1,14 @@
+
+<div id="index-banner" class="parallax-container">
+  <div class="section no-pad-bot">
+    <div class="container">
+      <br><br>
+      <h1 class="header center white-text text-lighten-2">OVA Confección</h1>
+      <div class="row center">
+        <h5 class="header col s12 ">Proceso de patronaje confección y acabado para una chaqueta vaquera</h5>
+      </div>
+      
+    </div>
+  </div>
+  <div class="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"></div>
+</div>
