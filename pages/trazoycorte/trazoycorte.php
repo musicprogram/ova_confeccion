3435e01402a17b222ffa7c72168e9895b407a0de
musicprogram <juan-o-henao@hotmail.es>
2018-04-21 11:37:02 -0500

    agregando vídeos a trazo y corte

--- pages/trazoycorte/trazoycorte.php
+++ pages/trazoycorte/trazoycorte.php
@@ -12,9 +12,9 @@
 
 
 
-		<blockquote class="white-text flow-text center-align">
-     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto unde porro sapiente voluptas corporis cumque totam consequuntur ab quibusdam, optio facere ea blanditiis, error repellendus reprehenderit asperiores impedit praesentium libero?
-    </blockquote>
+<blockquote class="white-text flow-text center-align">
+ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto unde porro sapiente voluptas corporis cumque totam consequuntur ab quibusdam, optio facere ea blanditiis, error repellendus reprehenderit asperiores impedit praesentium libero?
+</blockquote>
 
 
 
@@ -68,156 +68,322 @@
 <div class="card">
 	<div class="card-content">
 		
-      <table>
-        <thead>
-          <tr>
-              <th>vídeos de trazo y corte</th>
-             
-             	<th></th>
-          </tr>
-        </thead>
+    <table>
+      <thead>
+        <tr>
+          <th>vídeos de trazo y corte</th>
 
-        <tbody>
-          <tr id="ver_video">
-            
-          </tr>
+          <th></th>
+        </tr>
+      </thead>
 
-           <tr>
-            <td>Primer vídeo</td>
-         
-            <td>
-            	<a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="primer_video">
-            		<i class="material-icons Small">play_circle_outline</i>
-            	</a>
-            </td>
-          </tr>
-          <tr>
-            <td>Segundo vídeo</td>
-         
-            <td>
-            	<a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="segundo_video">
-            		<i class="material-icons Small">play_circle_outline</i>
-            	</a>
-            </td>
-          </tr>
+      <tbody>
+        <tr id="ver_video">
 
-          <tr>
-            <td>Tercer vídeo</td>
-         
-            <td>
-              <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="tercer_video">
-                <i class="material-icons Small">play_circle_outline</i>
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <td>Cuarto vídeo</td>
-         
-            <td>
-              <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="cuarto_video">
-                <i class="material-icons Small">play_circle_outline</i>
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <td>Quinto vídeo</td>
-         
-            <td>
-              <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="quinto_video">
-                <i class="material-icons Small">play_circle_outline</i>
-              </a>
-            </td>
-          </tr>
-          
-          <tr>
-            <td>Sexto vídeo</td>
-         
-            <td>
-              <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="sexto_video">
-                <i class="material-icons Small">play_circle_outline</i>
-              </a>
-            </td>
-          </tr>
+        </tr>
+
+        <tr>
+          <td>1 vídeo</td>
+
+          <td>
+           <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="primer_video">
+            <i class="material-icons Small">play_circle_outline</i>
+          </a>
+        </td>
+      </tr>
+      <tr>
+        <td>2 vídeo</td>
+
+        <td>
+         <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="segundo_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+
+    <tr>
+      <td>3 vídeo</td>
+
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="tercer_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+    <tr>
+      <td>4 vídeo</td>
+
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="cuarto_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+    <tr>
+      <td>5 vídeo</td>
+
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="quinto_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+
+    <tr>
+      <td>6 vídeo</td>
+
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="sexto_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+
+    <tr>
+      <td>7 vídeo</td>
+
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="septimo_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+
+    <tr>
+      <td>8 vídeo</td>
+
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="octavo_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+
+    <tr>
+      <td>9 vídeo</td>
+
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="noveno_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+
+
+
+    <tr>
+      <td>10 vídeo</td>
+
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="decimo_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+
+
 
   
 
-          
-        </tbody>
-      </table>
-	</div>
+
+    <tr>
+      <td>11 vídeo</td>
+      
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="doceavo_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+    
+    <tr>
+      <td>12 vídeo</td>
+      
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="treceavo">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>  
+    
+
+
+    <tr>
+      <td>13 vídeo</td>
+      
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="catorceavo">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>  
+    
+
+    <tr>
+      <td>14 vídeo</td>
+      
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="quinceavo">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>  
+
+
+
+    <tr>
+      <td>15  vídeo</td>
+      
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="dieciseisavo">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>  
+
+
+  <tr>
+      <td>16 vídeo</td>
+
+      <td>
+        <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1" href="#modal1" id="diesiocho_video">
+          <i class="material-icons Small">play_circle_outline</i>
+        </a>
+      </td>
+    </tr>
+
+
+
+  </tbody>
+</table>
 </div>
-            
+</div>
 
 
 
 
 
-	<!-- Modal Structure -->
+
+<!-- Modal Structure -->
 
 
-	<div id="modal1" class="modal green-text">
-		<div class="modal-content deep-purple accent-2">
-			<p><div class="video-container">
-				<iframe width="1102" height="620" src="" frameborder="0" allowfullscreen id="videos_confeccion"></iframe>
-			</div></p>
-		</div>
-		<div class="modal-footer  deep-purple accent-4">
-			<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat white-text">Cerrar</a>
-		</div>
-	</div>
+<div id="modal1" class="modal green-text">
+  <div class="modal-content deep-purple accent-2">
+   <p><div class="video-container">
+    <iframe width="1102" height="620" src="" frameborder="0" allowfullscreen id="videos_confeccion"></iframe>
+  </div></p>
+</div>
+<div class="modal-footer  deep-purple accent-4">
+ <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat white-text">Cerrar</a>
+</div>
+</div>
 
 
 
 
-	<script>
-		(function($){
-			$(function(){
-				$('.modal').modal();
-				$('#back').click(function(){
-					$('#main_content').load('pages/inicio/dashboard.php').slideDown(560); 
-					$('#titulo_principal').html('OVA Confección');    
-				});  
+<script>
+  (function($){
+   $(function(){
+    $('.modal').modal();
+    $('#back').click(function(){
+     $('#main_content').load('pages/inicio/dashboard.php').slideDown(560); 
+     $('#titulo_principal').html('OVA Confección');    
+   });  
 
-				$('#primer_video').click(function(){
-					$("#videos_confeccion").attr("src","https://www.youtube.com/embed/eTjDDjoi0AU");
-          ver_video();
+    $('#primer_video').click(function(){
+     $("#videos_confeccion").attr("src","https://www.youtube.com/embed/eTjDDjoi0AU");
+     ver_video();
 
-				})  
+   })  
 
-				$('#segundo_video').click(function(){
-					$("#videos_confeccion").attr("src","https://www.youtube.com/embed/mVEm3WXz08Y");
-          ver_video();
-				})
+    $('#segundo_video').click(function(){
+     $("#videos_confeccion").attr("src","https://www.youtube.com/embed/mVEm3WXz08Y");
+     ver_video();
+   })
 
-        $('#tercer_video').click(function(){
-          $("#videos_confeccion").attr("src","https://www.youtube.com/embed/GQD6qQ17f0Y");
-          ver_video();
-        })
+    $('#tercer_video').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/GQD6qQ17f0Y");
+      ver_video();
+    })
 
-        $('#cuarto_video').click(function(){
-          $("#videos_confeccion").attr("src","https://www.youtube.com/embed/E1HsaXSAJH8");
-          ver_video();
-        })
+    $('#cuarto_video').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/E1HsaXSAJH8");
+      ver_video();
+    })
 
-        $('#quinto_video').click(function(){
-          $("#videos_confeccion").attr("src","https://www.youtube.com/embed/Gg0bg1UqZ2A");
-          ver_video();
-        })
+    $('#quinto_video').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/Gg0bg1UqZ2A");
+      ver_video();
+    })
 
-        $('#sexto_video').click(function(){
-          $("#videos_confeccion").attr("src","https://www.youtube.com/embed/2ufW6eyeRCY?ecver=2");
-          ver_video();
-        })
+    $('#sexto_video').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/2ufW6eyeRCY?ecver=2");
+      ver_video();
+    })
 
-        
+    $('#septimo_video').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/pbVxEeeqBY0?ecver=2");
+      ver_video();
+    })
 
-        function ver_video(){
-          $('#ver_video').html('<td>seguir viendo el vídeo</td> <td> <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1 pulse" href="#modal1"> <i class="material-icons Small">play_circle_outline</i> </a> </td>');
-          
-        }
+    $('#octavo_video').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/XT8v-P7Pxn0?ecver=2");
+      ver_video();
+    })
 
-				/* cambiar el color del boton cuando se pasa de sección */
-				$("ul#list_videos li a i").click(function() {
+    $('#noveno_video').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/O2IcwX_paj0?ecver=2");
+      ver_video();
+    })
+
+    $('#decimo_video').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/Zy7VnXIwCKo?ecver=2");
+      ver_video();
+    })
+
+
+
+    $('#doceavo_video').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/rj__HThj_wE?ecver=2");
+      ver_video();
+    })
+
+    $('#treceavo').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/2dqca6fR1C8?ecver=2");
+      ver_video();
+    })
+
+    $('#catorceavo').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/6scDIFHuNdA?ecver=2");
+      ver_video();
+    })
+
+
+    $('#quinceavo').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/jdosEuWsjIs?ecver=2");
+      ver_video();
+    })
+
+    $('#dieciseisavo').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/oQZZfn7AaF8?ecver=2");
+      ver_video();
+    })
+
+
+    $('#diesiocho_video').click(function(){
+      $("#videos_confeccion").attr("src","https://www.youtube.com/embed/jS5MScexOTc?ecver=2");
+      ver_video();
+    })
+
+
+
+    function ver_video(){
+      $('#ver_video').html('<td>seguir viendo el vídeo</td> <td> <a class="waves-effect waves-light btn modal-trigger deep-purple lighten-1 pulse" href="#modal1"> <i class="material-icons Small">play_circle_outline</i> </a> </td>');
+
+    }
+
+    /* cambiar el color del boton cuando se pasa de sección */
+    $("ul#list_videos li a i").click(function() {
   	      // remove classes from all
   	      $("i").removeClass(" deep-purple-text text-darken-4");
   	      // add class to the one we clicked
@@ -233,12 +399,12 @@
 
 
 function pausar(){
-        $('#video_ama')[0].pause();
-      }
+  $('#video_ama')[0].pause();
+}
 
-      function play(){
-        $('#video_ama')[0].play();
-      }
+function play(){
+  $('#video_ama')[0].play();
+}
 
 
 
@@ -258,13 +424,13 @@ function pausar(){
 
 <script>
   (function($){
-  $(function(){
+    $(function(){
 
       $('#back').click(function(){
         $('#main_content').load('pages/inicio/dashboard.php').slideDown(560); 
         $('#titulo_principal').html('Ova Confección');    
       });  
- 
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
@@ -281,4 +447,3 @@ function pausar(){
 
 
 
-      
